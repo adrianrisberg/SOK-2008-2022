9cb0048bb594f5dd4ec860c7eace53b05cad170c
adrianrisberg <91265324+adrianrisberg@users.noreply.github.com>
2022-10-03 21:36:22 +0200

    Add files via upload

---
+++ Innlevering 3/Innlevering3R.R
@@ -0,0 +1,97 @@
+# Setter riktig 'working directory' 
+setwd("C:/Users/adria/OneDrive/Dokumenter/5. Semester/SOK-2008/Innlevering 3")
+
+# Velger norsk språk
+Sys.setlocale(locale="no_NO")
+
+# Laster nødvendige pakker
+library(readxl)
+library(ggplot2)
+
+# Utfordring 3.1.1
+
+ledighet <- read_excel("ledighet.xlsx", skip = 5, col_names = FALSE,
+                       n_max = 3)
+
+colnames(ledighet) <- c("Alder", "Utdanningsnivå", "Arbeidsledighet")
+
+ledighet <- ledighet[-2,-2]
+
+ggplot(ledighet, aes(Alder, Arbeidsledighet)) +
+  geom_bar(stat = "identity", color = "black", fill = "coral3") +
+  geom_text(aes(label = Arbeidsledighet),
+            vjust = 2, color = "white", size = 5) +
+  ylim(0, 15) +
+  labs(title = "3.1 - Arbeidsledighet i forskjellige aldersgrupper",
+       x = "Aldersgrupper",
+       y = "Arbeidsledighet (i %)") +
+  theme_light()
+
+# Kommentar i Word-dokument
+
+# Utfordring 3.1.2
+
+# We will use the following packages for the assignment: 
+library(OECD)   #The OECD package 
+library(tidyverse)  # the tidyverse package
+library(dplyr)  # The DPLYR package
+library(ggrepel) # The ggrepel package
+
+#We want to create a graph that shows the correlation between minimum wages and unemployment. We need to search the OECD data frame for data on these topics.
+#Search data set for minimum wages and unemployment statistics
+dsets<-get_datasets()
+search_dataset("wage",dsets)
+search_dataset("unemployment",dsets)
+
+#Data on minimum wages is available in "MIN2AVE"
+#Data on unemployment is available in "MIG_NUP_RATES_GENDER"
+
+#MinWage
+minwage <- get_dataset("MIN2AVE",
+                       filter = "USA+CAN+FRA+GBR+DEU+NZL", 
+                       pre_formatted = TRUE)
+#Selecting years and the min wage as a share of median wage
+minwage2019 <- subset(minwage, Time < 2019 & Time >2007 & SERIES=="MEDIAN")
+minwage2007_2019 <- subset(minwage2019, Time>2007)
+
+#UnEmpl
+unempl <- get_dataset("MIG_NUP_RATES_GENDER",
+                      filter = "USA+CAN+FRA+GBR+DEU+NZL", 
+                      pre_formatted = TRUE)
+
+#Selecting years, the unemployment rate of people born in the country, and both sexes
+unempl2019 <- subset(unempl, Time<2019 & RATE=="U_RATE" & BIRTH=="NB" & GENDER=="TOT")
+unempl2007_2019 <- subset(unempl2019, Time>2007)
+
+#Combining datasets - we need to merge by both country and year to get the right number in the right place
+minwage_unempl <-left_join(minwage2007_2019, unempl2007_2019, by=c("COUNTRY","Time"))
+
+#removing countries with missing data
+complete_minwage_unempl <- na.omit(minwage_unempl)
+
+#transforming the minimum wage and uneployment rate to numeric variables
+complete_minwage_unempl$MinWage_0 <-as.numeric(complete_minwage_unempl$ObsValue.x) #MinWage is between 0 and 1, I want to transform it to between 0 and 100 later, so I call it MinWage_0 here
+complete_minwage_unempl$UnEmpl <-as.numeric(complete_minwage_unempl$ObsValue.y)
+
+#Transforming Minimum wage to percent
+complete_minwage_unempl$MinWage <- complete_minwage_unempl$MinWage_0 * 100
+
+
+#Code for the graph (you need to insert data and variable names)
+ggplot(complete_minwage_unempl,
+       aes(UnEmpl, MinWage, group=COUNTRY, color=COUNTRY)) +
+  geom_line(aes(group=COUNTRY), size=1) +
+  geom_point(size=2.5)+
+  labs(title = "Sammenhengen mellom minstelønn som prosent av medianlønn\nog arbeidsledighet",
+       subtitle = "I prosent, Periode: 2008-2018",
+       x = "Arbeidsledighet i %",
+       y = "Minstelønn som prosent av medianlønn") +
+  theme(legend.position="none") +
+  geom_label_repel(data = complete_minwage_unempl %>% group_by(COUNTRY) %>%
+                     filter(UnEmpl==min(UnEmpl)),
+                   aes(UnEmpl, MinWage, fill = factor(COUNTRY),
+                       label = sprintf('%s', COUNTRY)),
+                   color = "black",
+                   fill = "white")
+
+
